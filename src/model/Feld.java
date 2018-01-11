f65b87661439dbeb37e873027d079e0630526a7a
Andreas Koessler <andreas.koessler@campus.lmu.de>
2018-01-11 11:18:36 +0100

    FEld und LEvel NEIghboursmethoden

--- src/model/Feld.java
+++ src/model/Feld.java
@@ -9,6 +9,10 @@ import java.util.Map;
 public class Feld {
     private Token token;
     private Map<Property, Integer> properties;
+    private int row;
+    private int column;
+    public enum Neighbour {LEFT, LEFTTOP, LEFTBOTTOM, RIGHT, RIGHTTOP, RIGHTBOTTOM, TOP, BOTTOM};
+    private Map<Neighbour, Feld> neighbours;
 
     public Feld(Token token) {
         this(token, new HashMap<Property, Integer>());
@@ -17,6 +21,17 @@ public class Feld {
     public Feld(Token token, Map<Property, Integer> properties){
         this.token = token;
         this.properties = properties;
+    }
+
+    public void setToken(Token token){
+        this.token = token;
+    }
+
+    public void setNeighbour(Neighbour neighbour, Feld field){
+        this.neighbours.put(neighbour,field);
+    }
+    public Feld getNeighbour(Neighbour neighbour) {
+        return this.neighbours.get(neighbour);
     }
 
     public Token getToken(){
