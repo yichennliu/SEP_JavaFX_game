45564650613ce6b03ccf12db55f7dd478f6c40e2
Immanuel Bach <bachi@cip.ifi.lmu.de>
2018-02-01 14:15:41 +0100

    Viewport wird angepasst bei veränderung der spielerposition

--- src/controller/GameController.java
+++ src/controller/GameController.java
@@ -14,6 +14,7 @@ import javafx.util.Duration;
 import main.LevelFactory;
 import model.enums.InputDirection;
 import model.enums.Property;
+import model.game.Feld;
 import model.game.Level;
 import view.GameView;
 
--- src/view/GameView.java
+++ src/view/GameView.java
@@ -1,5 +1,6 @@
 package view;
 
+import javafx.geometry.Point2D;
 import javafx.scene.Group;
 import javafx.scene.Scene;
 import javafx.scene.canvas.Canvas;
@@ -9,6 +10,7 @@ import javafx.scene.transform.Rotate;
 import javafx.scene.transform.Scale;
 import javafx.scene.transform.Translate;
 import javafx.stage.Stage;
+import model.game.Feld;
 import model.game.Level;
 import model.themeEditor.Theme;
 import model.themeEditor.ThemeIO;
@@ -27,7 +29,7 @@ public class GameView {
     private Theme theme;
     private Level level;
     private String stylesheet;
-    private double fieldSize = 15.0;
+    private double fieldSize = 60.0;
 
 
     public GraphicsContext getGameGC() {
@@ -51,18 +53,10 @@ public class GameView {
         stage.setTitle("BoulderDash - " + this.level.getName());
         this.update();
         this.theme = ThemeIO.importTheme("src/json/theme/testTheme.zip");
-        setInitialZoom();
+//        setInitialZoom();
         if(!stage.isShowing()) stage.show();
     }
 
-    private void setInitialZoom(){
-        int gameHeight = level.getMap().length;
-        int gameWidth =  level.getMap()[0].length;
-        if(gameWidth<=gameHeight)
-                zoom(1, gameCanvas.getWidth() / gameWidth/fieldSize);
-        else    zoom(1, gameCanvas.getHeight()/gameHeight/fieldSize);
-
-    }
 
     public Canvas getCanvas(){
         return this.gameCanvas;
@@ -77,12 +71,56 @@ public class GameView {
     }
 
     public void update(){
+        scrollToMe();
         View.drawMap(this.gameGC,level.getMap(),this.fieldSize, this.theme);
+
     }
 
     public void translate(double x, double y){
         this.transformation.append(new Translate(x,y));
         this.gameGC.setTransform(this.transformation);
+    }
+
+    private void scrollToMe(){
+        Feld meFeld = level.whereAmI();
+        if(meFeld==null) return;
+
+        double canvasHeight = gameCanvas.getHeight();
+        double canvasWidth = gameCanvas.getWidth();
+        double newTranslateX= 0;
+        double newTranslateY = 0;
+        double relativeMeX = meFeld.getColumn()*fieldSize + 0.5 * fieldSize;
+        double relativeMeY = meFeld.getRow()*fieldSize + 0.5 * fieldSize;
+        Point2D meOnCanvas= this.transformation.transform(relativeMeX,relativeMeY);
+        double meOnCanvasX = meOnCanvas.getX();
+        double meOnCanvasY = meOnCanvas.getY();
+
+        if(meOnCanvasX<canvasWidth*0.2 && reverseTransform(0,0).getX()>0) {
+            newTranslateX = canvasWidth*0.2 - meOnCanvasX;
+        }
+        if(meOnCanvasX>canvasWidth*0.8 && reverseTransform(canvasWidth,0).getX()<this.level.getWidth()*fieldSize) {
+            newTranslateX = canvasWidth*0.8 - meOnCanvasX;
+        }
+        if(meOnCanvasY<canvasHeight*0.2) {
+            newTranslateY = canvasHeight*0.2 - meOnCanvasY;
+        }
+        if(meOnCanvasY>canvasHeight*0.8){
+            newTranslateY = canvasHeight*0.8 - meOnCanvasY;
+        }
+
+
+           this.transformation.prepend(new Translate(newTranslateX,newTranslateY));
+        this.gameGC.setTransform(this.transformation);
+    }
+
+    /*gets a relative coordinate based on given input and affine transformation parameters*/
+    private Point2D reverseTransform(double j, double k){
+        double a = transformation.getMxx(); double b = transformation.getMxy();
+        double c = transformation.getTx(); double d = transformation.getMyx();
+        double e = transformation.getMyy(); double f = transformation.getTy();
+        double x = (b*f - b*k - c*e +e*j) / (a*e - b*d);
+        double y = ( a*k + c*d - d*j -a*f) / (a*e -b*d);
+        return new Point2D(x,y);
     }
 
     public void zoom(double delta, double factor){
