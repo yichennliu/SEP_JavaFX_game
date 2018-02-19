6aa9532bd3c3601a4cc956bb031d5d1692a009fe
Andreas Koessler <andreas.koessler@campus.lmu.de>
2018-02-19 11:01:36 +0100

    Code cleaning

--- src/model/game/Rule.java
+++ src/model/game/Rule.java
@@ -6,7 +6,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 import static model.enums.Direction.*;
-import static model.enums.Situation.ANY;
+import static model.enums.Situation.RARE;
 
 public class Rule {
 
@@ -29,24 +29,27 @@ public class Rule {
 
         int i = 0;
         int startJ = getStartJ(map, direction);
-        int   endJ = getEndJ(map, direction);
+        int endJ = getEndJ(map, direction);
         int startI = 0;
-        int   endI = getEndI(map, direction);
+        int endI = getEndI(map, direction);
         int ruleLength = original.size();
+
 
         Situation inputSituation = (inputD != null) ? inputD.getSituation() : null;
 
-        if (!isRightSituation(inputSituation)) {return;}
+        if (!isRightSituation(inputSituation)) {
+            return;
+        }
 
-        while (between(startI,i,endI)) {
+        while (between(startI, i, endI)) {
 
-            int j = ((direction == SOUTH || direction == EAST) ? 0 : (direction == NORTH) ? map.length-1 : map[0].length-1);
+            int j = initJSE(map);
 
-            while (between(startJ,j,endJ)) {
+            while (between(startJ, j, endJ)) {
                 List<Feld> feldList = new ArrayList();
                 try {
-                    int y = (direction == EAST || direction == WEST) ? i : j;
-                    int x = (direction == NORTH || direction == SOUTH) ? i : j;
+                    int y = initY(i, j);
+                    int x = initX(i, j);
                     feldList.add(map[y][x]);
                     map[y][x].getNeighboursRecursive(direction, ruleLength, feldList);
 
@@ -54,15 +57,12 @@ public class Rule {
                         for (int z = 0; z <= ruleLength; z++) {
                             this.result.get(z).replace(feldList.get(z), feldList);
                         }
-
-                        j = (direction == NORTH || direction == WEST) ? j - ruleLength - 1 : j + ruleLength + 1;
-                        if (!between(startJ,j,endJ))
+                        j = hoverOverRulelengthJNW(ruleLength, j);
+                        if (!between(startJ, j, endJ))
                             throw new IndexOutOfBoundsException();
                     } else {
-
-                        j = (direction == NORTH || direction == WEST) ? j-1  : j+1;
+                        j = incOrDecJNW(j);
                     }
-
                 } catch (IndexOutOfBoundsException e) {
                     break;
                 }
@@ -70,7 +70,7 @@ public class Rule {
             i++;
         }
 
-        if (inputSituation == Situation.RARE && this.situation == Situation.RARE && this.sparsity != null) {
+        if (decSparsity(inputSituation)) {
             this.sparsity -= 1;
         }
     }
@@ -79,7 +79,7 @@ public class Rule {
 
         if (direction == NORTH) {
             return map.length - 1;
-        } else if(direction == WEST){
+        } else if (direction == WEST) {
             return map[0].length - 1;
         } else {
             return 0;
@@ -89,37 +89,57 @@ public class Rule {
     private int getEndJ(Feld[][] map, Direction direction) {
 
         if (direction == EAST) {
-            return map[0].length-1;
-        } else if(direction == SOUTH) {
+            return map[0].length - 1;
+        } else if (direction == SOUTH) {
             return map.length - 1;
         } else return 0;
     }
 
-    private int getEndI(Feld[][] map, Direction direction){
+    private int getEndI(Feld[][] map, Direction direction) {
 
-        if(direction == NORTH||direction == SOUTH)
-            return map[0].length -1;
+        if (direction == NORTH || direction == SOUTH)
+            return map[0].length - 1;
         else
-            return map.length -1;
+            return map.length - 1;
     }
 
     private boolean isRightSituation(Situation inputSituation) {
         if (this.situation == Situation.ANY) return true;
-        if (this.situation == Situation.RARE && this.sparsity != null && this.sparsity.intValue() > 0) return true;
+        if (this.situation == RARE && this.sparsity != null && this.sparsity.intValue() > 0) return true;
         if (inputSituation != null && inputSituation.equals(this.situation)) return true;
         return false;
     }
 
     /*überprüft, ob b zwischen a und c ist*/
-    private boolean between(int a, int b, int c){
-        if(c>a) {
+    private boolean between(int a, int b, int c) {
+        if (c > a) {
             return (b >= a && b <= c);
         }
-        if(a>c){
-            return (b>=c && b<=a);
+        if (a > c) {
+            return (b >= c && b <= a);
         }
 
-        return(b==c);
+        return (b == c);
+    }
+
+    private int initJSE(Feld[][] map) {
+        return ((direction == SOUTH || direction == EAST) ? 0 : (direction == NORTH) ? map.length - 1 : map[0].length - 1);
+    }
+
+    private int hoverOverRulelengthJNW(int ruleLength, int j) {
+        return (direction == NORTH || direction == WEST) ? j - ruleLength - 1 : j + ruleLength + 1;
+    }
+
+    private int incOrDecJNW(int j) {
+        return (direction == NORTH || direction == WEST) ? j - 1 : j + 1;
+    }
+
+    private int initX(int i, int j) {
+        return (direction == NORTH || direction == SOUTH) ? i : j;
+    }
+
+    private int initY(int i, int j) {
+        return (direction == EAST || direction == WEST) ? i : j;
     }
 
     private boolean matchFeldListWithOriginalList(List<Feld> list) {
@@ -128,6 +148,12 @@ public class Rule {
             if (!ruleElmnt.matches(list.get(k))) return false;
         }
         return true;
+    }
+
+    private boolean decSparsity(Situation inputSituation){
+        if((inputSituation == RARE &&this.situation == RARE &&this.sparsity !=null))
+            return true;
+        return false;
     }
 
     public int getSparsity() {
