260aa8aa42d4aa42fd79ac2a8303fdfd3948c08b
Immanuel Bach <bachi@cip.ifi.lmu.de>
2018-02-20 13:05:49 +0100

    verbesserungen im levelEditor

--- src/controller/GameController.java
+++ src/controller/GameController.java
@@ -54,7 +54,7 @@ public class GameController {
         this.addDirectionEvents();
         this.addInGameMenu();
         this.robot = new Robot(level,5);
-        robotize(true);
+        robotize(false);
         this.addGameViewComponents();
         this.countDown();
     }
